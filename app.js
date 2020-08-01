0b6c6ec19b9e2c1d9a5c7d7858a5ef4d8a6b7031
Olga <olga.filiushkina@gmail.com>
2020-07-31 22:40:22 -0400

    controls are now hidden until the game is started. added a general function to calculate damage

--- app.js
+++ app.js
@@ -2,54 +2,71 @@
 // add color classes to each turn
 
 //// "Start New Game" => set both health bars to 100 (styles width: 100%) and clear the log + append message "new game!". 
+////  hide the new game button and reveal control buttons
 
-//// "Attack" => decrease your health by a random # (1-20) and add "You lost X health" to the log. decrease monster's health by a random # and add "Monster took X damage" to the log.
+//// "Attack" => decrease player health by a random # (1-20) and add "You lost X health" to the log. decrease monster's health by a random # and add "Monster took X damage" to the log.
 //// I need a separate randomizer function (D20)
 
 
-//// "Special attack" (not sure about project requirements) => decrease monster's health by random # x2 and add "Monster took X damage". Decrease your health same as "Attack" button. 
+//// "Special attack" (not sure about project requirements) => decrease monster's health by random # x2 and add "Monster took X damage". Decrease player health same as "Attack" button. 
 //Bonus: add timeout for this button
 
 //Try to refactor 'Attack' and 'Special attack'
 
-//// "Heal" => increase your health by a random number (5-20). 
+//// "Heal" => increase player health by a random number (5-20). 
 // bonus: add timeout
 
 //// "Give up" => print "You gave up!" in the log
 
-//// If your health gets to 0, print "You loose!"
+//// If player health gets to 0, print "You loose!"
 //// If monster health gets to 0, print "You win!"
 //// instead of printing it to the console, save it in state and display a message based on that state using v-if
 //// Where do I check this? Do I need to use v-if?
 
-// disable buttons when the game is won or lost?
+// disable buttons when the game is won or lost? or display a modal
 
 
 
 new Vue({
   el: '#app',
   data: {
-    yourHealth: 100,
+    playerHealth: 100,
     monsterHealth: 100,
+    gameIsRunning: false,
     log: ['Time to fight!'],
-    youWon: false,
-    monsterWon: false,
   },
   methods: {
-    rollD20: function() {
-      return Math.floor(Math.random() * 20 + 1)
+    calculateDamage: function(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
     },
-    winnerCheck: function() {
-      if (this.yourHealth <= 0) {
-        this.monsterWon = !this.monsterWon;
-        this.yourHealth = 0;
+    checkWin: function() {
+      // checks if player or monster's health is depleted and if yes, ends the game
+      if (this.playerHealth <= 0) {
+        
+        this.playerHealth = 0;
+
+        if (confirm('Monster won! New Game?')) {
+          // creates a confirmation dialogue that allows to restart the game
+          this.startNewGame();
+        } else {
+          this.gameIsRunning = false;
+        }
       } else if (this.monsterHealth <= 0) {
-        this.youWon = !this.youWon;
+        
         this.monsterHealth = 0;
+
+        if (confirm('You won! New Game?')) {
+          // creates a confirmation dialogue that allows to restart the game
+          this.startNewGame();
+        } else {
+          this.gameIsRunning = false;
+        }
       }
     },
     startNewGame: function() {
-      this.yourHealth = 100;
+      // resets everything
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
       this.monsterHealth = 100;
       this.log = ['Time to fight!'];
       this.youWon = false;
@@ -57,38 +74,42 @@ new Vue({
     },
     attack: function() {
       // decreases the players and monster's health by a random number between 1 and 20
-      const yourDamage = this.rollD20();
-      const monsterDamage = this.rollD20();
-      this.yourHealth -= yourDamage;
-      this.monsterHealth -= monsterDamage;
-      this.log.push(`You lost ${yourDamage} health!`)
-      this.log.push(`The monster took ${monsterDamage} damage!`);
-      this.winnerCheck();
+      const damageToPlayer = this.calculateDamage(5, 30);
+      this.playerHealth -= damageToPlayer;
+      this.log.push(`You lost ${damageToPlayer} health!`)
+
+      const damageToMonster = this.calculateDamage(1, 20);
+      this.monsterHealth -= damageToMonster;
+      this.log.push(`The monster took ${damageToMonster} damage!`);
+
+      this.checkWin();
     },
     specialAttack: function() {
-      // same as "attack" but player deals twice the damage
-      const yourDamage = this.rollD20() ;
-      const monsterDamage = this.rollD20() * 2;
-      this.yourHealth -= yourDamage;
-      this.monsterHealth -= monsterDamage;
-      this.log.push(`You lost ${yourDamage} health!`)
-      this.log.push(`Your special attack deals ${monsterDamage} damage!`);
-      this.winnerCheck();
+      // same as "attack" but player deals more damage
+      const damageToPlayer = this.calculateDamage(10, 30);
+      this.playerHealth -= damageToPlayer;
+      this.log.push(`You lost ${damageToPlayer} health!`)
+
+      const damageToMonster = this.calculateDamage(5, 30);
+      this.monsterHealth -= damageToMonster;
+      this.log.push(`Your special attack deals ${damageToMonster} damage!`);
+
+      this.checkWin();
     },
     heal: function() {
       // heals 5-20 health points
       const healthGain = Math.floor(Math.random() * (20 - 5 + 1)) + 5;
       //This prevents the player's health bar going over 100
-      if (this.yourHealth + healthGain >= 100) {
-        this.log.push(`You healed ${100 - this.yourHealth} health.`);
-        this.yourHealth = 100;
+      if (this.playerHealth + healthGain >= 100) {
+        this.log.push(`You healed ${100 - this.playerHealth} health.`);
+        this.playerHealth = 100;
       } else {
-        this.yourHealth += healthGain;
+        this.playerHealth += healthGain;
         this.log.push(`You healed ${healthGain} health.`);
       }
     },
     giveUp: function() {
-      this.yourHealth = 0;
+      this.playerHealth = 0;
       this.log.push(`You gave up and lost the game!`);
     }
   }
